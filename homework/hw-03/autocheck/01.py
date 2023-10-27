333d263ad5654f1e0661655d1144a6dbb6603435
Pavlo <105214761+PavloDavydenko@users.noreply.github.com>
2023-10-27 11:43:28 +0300

    hw-03 autocheck 01.py

---
+++ homework/hw-03/autocheck/01.py
@@ -0,0 +1,14 @@
+"""
+Створіть функцію greeting, яка всередині виводить вітальне повідомлення print('Hello world!'). І викличте її.
+"""
+
+def greeting():
+    print('Hello world!')
+
+
+def main():
+    greeting()
+
+
+if __name__ == "__main__":
+    main()
