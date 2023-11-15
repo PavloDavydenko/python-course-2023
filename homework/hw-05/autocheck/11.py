31945942b45b1587582f4f18f9e0ee346b89971e
Pavlo <105214761+PavloDavydenko@users.noreply.github.com>
2023-11-16 01:34:17 +0200

    hw-05 autocheck 11.py

---
+++ homework/hw-05/autocheck/11.py
@@ -0,0 +1,24 @@
+"""
+Для закріплення матеріалу напишіть функцію find_all_words, яка шукає збіг слова в тексті. 
+Функція повертає список всіх знаходжень слова в параметрі word в тексті у вигляді будь-якого написання, тобто, 
+наприклад, можливі варіанти написання слова "Python" як pYthoN, pythOn, PYTHOn і т.і. головне, щоб зберігався порядок слів, регістр не має значення.
+
+Підказка: функції модуля re приймають ще ключовий параметр flags і ми можемо визначити нечутливість до регістру, надавши йому значення re.IGNORECASE
+"""
+
+import re
+
+
+def find_all_words(text, word) -> list:
+    return re.findall(word, text, flags=re.IGNORECASE)
+    
+
+def main():
+    text = "Guido van Rossum began working on PYTHON in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0."
+    word = "Python"
+
+    print(find_all_words(text, word))
+
+
+if __name__ == "__main__":
+    main()
